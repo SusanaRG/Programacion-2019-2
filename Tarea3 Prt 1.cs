4fe9187df15f9f88ff77af21ba5d666cfbe21eb2
SusanaRG <53104721+SusanaRG@users.noreply.github.com>
2019-07-28 18:10:40 -0500

    Add files via upload

---
+++ Tarea3 Prt 1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3_parte_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Datos
+            Console.WriteLine("Ingrese su salario mensual: ");
+            double s = double.Parse(Console.ReadLine());
+
+            //operaciones
+            double smmlv = 828.116;
+
+            if (s < (2 * smmlv))
+            {
+                Console.WriteLine("Usdted tiene acceso a la Tarifa A");
+            }
+            else if ((2 * smmlv) < s && s < (4 * smmlv))
+            {
+                Console.WriteLine("Usdte tiene acceso a la Tarifa B");
+            } 
+            else
+            {
+                Console.WriteLine("Usdted tiene acceso a la Tarifa c");
+            }
+        }
+    }
+}
---
+++ Tarea3 Prt 2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3_parte_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Datos 
+            Console.WriteLine("Ingrese su salario mensual: ");
+            double s = double.Parse(Console.ReadLine());
+
+            //calculos 
+            double smmlv = 828.116;
+
+            if (s < (2 * smmlv))
+            {
+                Console.WriteLine("Tiene acceso a la Tarifa A y debe pagar un monto de 3.200");
+            } 
+            else if ((2 * smmlv) < s && s < (5 * smmlv))
+            {
+                Console.WriteLine("Tiene acceso a la Tarifa B y debe pagar un monto de 12.700");
+            }
+            else
+            {
+                Console.WriteLine("Tiene acceso a la Tarifa C y debe pagar un monto de 33.700");
+            }
+
+
+        }
+    }
+}
