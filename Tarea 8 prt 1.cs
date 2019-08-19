b9d215e8e2b7021c3926066a6d1a60ad2001ffb0
SusanaRG <53104721+SusanaRG@users.noreply.github.com>
2019-08-19 15:03:39 -0500

    Add files via upload

---
+++ Tarea 8 prt 1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int dado = 0, total = 0, contador = 0, contador2 = 0, vidas = 3;
+            string arrojar = "s";
+
+
+            while (arrojar == "s" && vidas > 0)
+            {
+                Console.WriteLine("Arrojar dados (s/n): ");
+                arrojar = Console.ReadLine();
+                dado = aleatorio.Next(1, 7);
+                Console.Write("Dado = " + dado);
+
+                total += dado;
+                Console.Write("Total = " + total);
+
+                Console.Write("vidas =" + vidas);
+
+                if (dado == 1)
+                {
+                    contador += 1;
+                    if (contador % 2 == 0 && vidas > 0)
+                    {
+                        vidas -= 1;
+                        total -= 10;
+                    }
+                }
+
+                if (dado == 6) contador2 += 1;
+                if (dado != 6 && contador2 > 0) contador2 -= 1;
+                if (contador2 == 2) vidas += 1;
+
+                if (vidas > 3) vidas = 3;
+                if (vidas == 0) Console.WriteLine("No tienes más vidas, has perdido");
+
+                Console.WriteLine("Gracias por jugar");
+                Console.WriteLine("Su total fue de " + total + "puntos");
+            }
+        }
+    }
+}
---
+++ Tarea 8 prt 2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_8_parte_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int  dado1 = 0, total = 0, contador = 0, vidas = 3;
+            string arrojar = "s";
+
+            while(arrojar == "s" && vidas > 0)
+            {
+                Console.WriteLine("¿ Arrojar dados ? (s/n): ");
+                arrojar = Console.ReadLine();
+                dado1 = aleatorio.Next(1, 7);
+                Console.Write("\n Dado = " + dado1);
+
+                total += dado1;
+                Console.Write("\n Total = " + total);
+
+                Console.Write("\n vidas =" + vidas);
+
+                if (arrojar == "s")
+                {
+                    contador += 1;
+                    if (contador % 2 == 0 && vidas > 0) vidas -= 1;
+                }
+
+                if (vidas == 0)
+                {
+                    Console.WriteLine("No tienes más vidas, has perdido");
+                    break;
+                }
+
+                if (arrojar == "s" && contador % 3 == 0)
+                    {
+                    int dado2 = aleatorio.Next(1, 7);
+                    Console.Write("\n Dado2 = " + dado2);
+                    total += dado2;
+                    Console.Write("\n Total = " + total);
+                    Console.Write("\n vidas =" + vidas);
+                    
+                    if (dado1 == dado2) vidas += 1;
+                    }
+                
+
+                if (vidas > 3) vidas = 3;
+
+
+            }
+
+        }
+    }
+}
